90e063219658fcdf81064dbdc7b6de607b8e47b1
Anshul M <anshmaske743@gmail.com>
2022-12-02 00:44:25 -0500

    API boilerplate code

---
+++ api.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/greet', methods=['GET', 'POST'])
+def greet():
+    data = {
+        'msg': 'Hello'
+    }
+    response = jsonify(data)
+    response.status_code = 200
+    return response
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
