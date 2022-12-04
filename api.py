218b15c4667bbf46bde99532323a45050d890a60
Anshul M <anshmaske743@gmail.com>
2022-12-04 17:38:05 -0500

    api can now extract articles

--- api.py
+++ api.py
@@ -1,20 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, render_template_string, request, jsonify
+from extractor import get_article
 
 app = Flask(__name__)
 
-@app.route('/greet', methods=['GET', 'POST'])
-def greet():
-    data = {
-        'msg': 'Hello'
-    }
-    response = jsonify(data)
-    response.status_code = 200
-    return response
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template_string("<h1>Hello</h1>")
 
-@app.route('/extract', methods=['GET', 'POST'])
+@app.route('/extract/', methods=['GET', 'POST'])
 def extract():
-    data = {}
-
+    url = r'https://web.archive.org/web/2013030319id_/http://online.wsj.com/article/SB10001424127887323940004578255810468323252.html'
+    data = {
+        'text': get_article(url=str(url))
+        }
     response = jsonify(data)
     response.status_code = 200
     return response
